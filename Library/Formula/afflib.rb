71faa8197dd2b56d1144c6140a03b99e84e9a0c9
Bastien Jaillot <bastien@jaillot.org>
2011-01-15 23:50:31 +0100

    revert conflict

--- Library/Formula/afflib.rb
+++ Library/Formula/afflib.rb
@@ -1,15 +1,9 @@
 require 'formula'
 
 class Afflib <Formula
-<<<<<<< HEAD
-  url 'http://afflib.org/downloads/afflib-3.6.4.tar.gz'
-  homepage 'http://afflib.org'
-  md5 'bc03dcd98b8a1d04169b6261ce304458'
-=======
   url 'http://afflib.org/downloads/afflib-3.6.6.tar.gz'
   homepage 'http://afflib.org'
   md5 'b7ff4d2945882018eb1536cad182ad01'
->>>>>>> 4d115cf1aa2d86c492e34f7a2481ee79180d352a
 
   depends_on 'expat' => :optional
 
